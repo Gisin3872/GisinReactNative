2fd8bf55427608ef3f4c555867a4dabd4c64fa95
Gisin3872 <matthewgisin@gmail.com>
2024-04-11 13:05:04 -0400

    Fixed justify

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 const App = () => {
   return (
-    <View style={{ flex: 1, jutifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Matt Gisin</Text>
     </View>
   );
